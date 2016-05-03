eb2780a03bb000ccd0a5e618b7342ed6f71234ea
Jared Billig <jared.billig@gmail.com>
2016-05-03 13:54:33 -0400

    queries file for db

---
+++ db/queries.js
@@ -0,0 +1,15 @@
+var knex = require('./knex.js');
+
+function Shows() {
+  return knex('shows');
+}
+
+// *** queries *** //
+
+function getAll() {
+  return Shows().select();
+}
+
+module.exports = {
+  getAll: getAll
+};
