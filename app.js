6aa4c7fe4b1691c4fb0f76015871ea24822cacfc
Jared Billig <jared.billig@gmail.com>
2016-05-03 13:38:30 -0400

    changed error handlers to send back json instead

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    res.json({
+      message : err.message,
+      error   : err
     });
   });
 }
@@ -52,7 +52,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
